7eb0ef16db48c072dd4d8405a54c41969f46f8ee
Alan Bertani <alanbertani051@gmail.com>
2024-08-23 20:10:44 -0300

    add: implement verbose mode

--- cli/src/cli.ts
+++ cli/src/cli.ts
@@ -14,12 +14,20 @@ program
   .argument('<phrase>', 'The phrase to be analyzed')
   .action((phrase: string, options) => {
     const { depth, verbose } = options;
+    const startTime = process.hrtime();
 
     const normalized_phrase = trie.normalize(phrase).split(" ");
     const analysisResult = trie.analyzeAtDepth(depth, normalized_phrase);
     console.log(analysisResult);
 
+    const paramLoadTime = process.hrtime(startTime);
+    const analysisStartTime = process.hrtime();
+    const analysisTime = process.hrtime(analysisStartTime);
 
+    if (verbose) {
+      console.log(`Tempo de carregamento dos parâmetros | ${(paramLoadTime[0] * 1000 + paramLoadTime[1] / 1e6).toFixed(3)}ms`);
+      console.log(`Tempo de verificação da frase        | ${(analysisTime[0] * 1000 + analysisTime[1] / 1e6).toFixed(3)}ms`);
+    }
   });
 
 program.parse(process.argv);
