a1d264e59557a603b4a8c9f8ccd51979b6a2c636
Alan Bertani <alanbertani051@gmail.com>
2024-08-25 00:57:38 -0300

    add: change placeholder name

--- web/src/components/AddChildInput.tsx
+++ web/src/components/AddChildInput.tsx
@@ -8,14 +8,14 @@ export const AddChildInput: React.FC<{
       type="text"
       value={newChildName}
       onChange={(e) => setNewChildName(e.target.value)}
-      placeholder="New child node name"
+      placeholder="Nome do novo nó"
       className="border rounded px-2 py-1 mr-2"
     />
     <button
       onClick={handleAddChild}
       className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded"
     >
-      Add
+      Adicionar
     </button>
   </div>
 );
